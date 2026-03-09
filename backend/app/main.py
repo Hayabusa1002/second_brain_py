1cc8b123f54eb4643cbd79134ffa9645e4a5096f
Luis Daniel <ldfg1002@gmail.com>
2026-03-08 21:07:04 -0500

    backend: balance calculation service

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 
-from app.routers import transactions, categories
+from app.routers import transactions, categories, accounts
 
 app = FastAPI()
 
 app.include_router(transactions.router)
 app.include_router(categories.router)
+app.include_router(accounts.router)
