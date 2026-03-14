c395c9b960aeac0a3d5f95502ca98aa7c33dc37a
Luis Daniel <ldfg1002@gmail.com>
2026-03-13 21:41:43 -0500

    bug: individual vs shared view not updated app/main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,12 +2,13 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 
-from app.routers import transactions, categories
+from app.routers import transactions, categories, accounts
 
 app = FastAPI()
 
 app.include_router(transactions.router)
 app.include_router(categories.router)
+app.include_router(accounts.router)
 
 app.mount(
     "/static",
