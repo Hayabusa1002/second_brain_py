6d32863adb894bdbc0ce79ecb8014f35452aea81
Luis Daniel <ldfg1002@gmail.com>
2026-03-15 01:10:25 -0500

    bug: reset alembic version and avoid conflict in 5432 port

--- docs/features/docker.bash
+++ docs/features/docker.bash
@@ -1,5 +1,12 @@
 # first time
-docker compose up --build
+docker compose up --build -d
+
+# (If needed) for reset alembic versions and save it in local
+docker compose exec app alembic revision --autogenerate -m "initial schema"
+docker cp second_brain-app-1:/app/migrations/versions/. ./migrations/versions/
+
+# (If needed) for ghost register
+docker compose exec db psql -U postgres -d second_brain -c "DELETE FROM alembic_version;"
 
 # Run Alembic migrations inside docker
 docker compose exec app alembic upgrade head
