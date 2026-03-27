9889e14c41b2a19ab90084fec93775f4a7025b34
Luis Daniel <ldfg1002@gmail.com>
2026-03-27 18:15:19 -0500

    bug: description field wasn't created

--- backend/app/models/transaction.py
+++ backend/app/models/transaction.py
@@ -2,7 +2,7 @@ import uuid
 import enum
 from datetime import datetime, UTC
 
-from sqlalchemy import Column, Date, DateTime, Enum, ForeignKey, Numeric
+from sqlalchemy import Column, Date, DateTime, Enum, ForeignKey, Numeric, String
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 
@@ -21,6 +21,7 @@ class Transaction(Base):
     created_by  = Column(UUID(as_uuid=True), ForeignKey("users.id"),       nullable=False)
     amount      = Column(Numeric(12, 2), nullable=False)
     type        = Column(Enum(TransactionType), nullable=False)
+    description = Column(String, nullable=True)
     date        = Column(Date, nullable=False)
     created_at  = Column(DateTime(timezone=True), default=lambda: datetime.now(UTC))
 
