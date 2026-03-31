c70ff307647cd406989a4920cea1928330cfcc02
Luis Daniel <ldfg1002@gmail.com>
2026-03-30 20:30:26 -0500

    frontend: login more like tabler

--- frontend/app/src/components/ui/Card.jsx
+++ frontend/app/src/components/ui/Card.jsx
@@ -1,8 +1,7 @@
-export default function Card({ title, children }) {
+export default function Card({ children }) {
   return (
     <div className="card card-md">
       <div className="card-body">
-        {title && <h2 className="card-title text-center mb-4">{title}</h2>}
         {children}
       </div>
     </div>
